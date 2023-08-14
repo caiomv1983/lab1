25e539055d97e2afe1a158a92a9077a8ccf0cd5e
caiomv1983 <125095286+caiomv1983@users.noreply.github.com>
2023-08-14 14:54:46 -0300

    Create exercicio1.c

---
+++ exercicio1.c
@@ -0,0 +1,16 @@
+/* Lab 1
+  * Exemplo 1*/
+ 
+#include <stdio.h>
+
+int main() {
+	// imprime algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Ciencias da Computacao - ");
+	printf("Turma 02N11\n");
+	
+
+	return 0;
+} 
